6e1ae36713e3594a5bf179a2f1edcec560f168ff
Bryan <bryanlima200@outlook.com>
2021-03-08 20:13:20 -0300

    Configured stack class

--- 01-Program-Structure/Stack.cs
+++ 01-Program-Structure/Stack.cs
@@ -1,9 +1,32 @@
 using System;
 
-namespace ProgramStructure
-{
-    public class Stack
-    {
-        
+namespace ProgramStructure {
+    public class Stack {
+        Position first;
+
+        public void StackUp(object item) {
+            first = new Position(first, item);
+        }
+
+        public object Unstack() {
+            if(first == null) {
+                throw new InvalidOperationException();
+            }
+
+            object result = first.item;
+            first = first.next;
+            return result;
+        }
+
+        class Position {
+            public Position next;
+
+            public object item;
+
+            public Position(Position next, object item) {
+                this.next = next;
+                this.item = item;
+            }
+        }
     }
 }
