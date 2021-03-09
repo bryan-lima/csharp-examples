6153de2838566fbafa10f28b97b47f9d39295b09
Bryan <bryanlima200@outlook.com>
2021-03-09 16:57:56 -0300

    Added add student functionality

--- Revision/Program.cs
+++ Revision/Program.cs
@@ -6,6 +6,9 @@ namespace Revision
     {
         static void Main(string[] args)
         {
+            Student[] students = new Student[5];
+            var studentIndex = 0;
+
             string userOption = GetUserOption();
 
             while (userOption.ToUpper() != "X")
@@ -13,8 +16,24 @@ namespace Revision
                 switch (userOption)
                 {
                     case "1":
-                        // TODO: Add student
-                        Console.WriteLine($"Você digitou a opção {userOption}");
+                        Console.WriteLine("Informe o nome do aluno:");
+                        Student student = new Student();
+                        student.Name = Console.ReadLine();
+                        Console.WriteLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+                        if (decimal.TryParse(Console.ReadLine(), out decimal note))
+                        {
+                            student.Note = note;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Valor da nota deve ser decimal!");
+                        }
+
+                        students[studentIndex] = student;
+                        studentIndex++;
+
                         break;
                     case "2":
                         // TODO: List students
@@ -35,14 +54,18 @@ namespace Revision
         private static string GetUserOption()
         {
             Console.WriteLine();
-            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(" Informe a opção desejada: ");
+            Console.WriteLine("---------------------------");
             Console.WriteLine("1 - Inserir novo aluno");
             Console.WriteLine("2 - Listar alunos");
             Console.WriteLine("3 - Calcular média geral");
             Console.WriteLine("X - Sair");
+            Console.WriteLine("---------------------------");
             Console.WriteLine();
 
             string userOption = Console.ReadLine();
+            Console.WriteLine();
             return userOption;
         }
     }
