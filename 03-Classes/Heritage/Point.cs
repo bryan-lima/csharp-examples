8ffc166026cfdc89ab2510255d37b94bb7806185
Bryan <bryanlima200@outlook.com>
2021-03-09 02:29:24 -0300

    Configured Point class

--- 03-Classes/Heritage/Point.cs
+++ 03-Classes/Heritage/Point.cs
@@ -2,6 +2,29 @@ namespace Classes.Heritage
 {
     public class Point
     {
-        
+        public int x, y;
+
+        private int distance;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        protected void CalculateDistance()
+        {
+            // Do something...
+        }
+
+        private void CalculateDistance2()
+        {
+            // Do something...
+        }
+
+        public virtual void CalculateDistance3()
+        {
+            // Do something...
+        }
     }
 }
