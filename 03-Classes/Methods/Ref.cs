f1a07b7df6a550fc06f4a04a78d6237b89e75ed9
Bryan <bryanlima200@outlook.com>
2021-03-09 14:41:40 -0300

    Configured Ref class

--- 03-Classes/Methods/Ref.cs
+++ 03-Classes/Methods/Ref.cs
@@ -1,7 +1,20 @@
-namespace Classes.Methods;
+namespace Classes.Methods
 {
     public class Ref
     {
+        static void Reverse(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static void Reverse()
+        {
+            int i = 1, j = 2;
+            Reverse(ref i, ref j);
+            System.Console.WriteLine($"{i} {j}");  // Write "2 1"
+        }
         
     }
 }
