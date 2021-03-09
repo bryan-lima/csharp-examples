b07d83c6da568a8e31ad5467482057529d4fb11f
Bryan <bryanlima200@outlook.com>
2021-03-09 14:30:54 -0300

    Configured Point3D class

--- 03-Classes/Heritage/Point3D.cs
+++ 03-Classes/Heritage/Point3D.cs
@@ -1,7 +1,23 @@
 namespace Classes.Heritage
 {
-    public class Point3D
+    public class Point3D : Point
     {
+        public int z;
 
+        public Point3D(int x, int y, int z) : base(x, y)
+        {
+            this.z = z;
+            CalculateDistance();
+        }
+
+        public static void Calculate()
+        {
+            // Do something...
+        }
+
+        public override void CalculateDistance3()
+        {
+            // Do something...
+        }
     }
 }
