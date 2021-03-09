ab7a084eb85f4dfb1bd2651343e2a49b68b22e64
Bryan <bryanlima200@outlook.com>
2021-03-09 14:56:10 -0300

    Configured Out class

--- 03-Classes/Methods/Out.cs
+++ 03-Classes/Methods/Out.cs
@@ -2,6 +2,16 @@ namespace Classes.Methods
 {
     public class Out
     {
-        
+        static void Divide(int x, int y, out int result, out int rest)
+        {
+            result = x / y;
+            rest = x % y;
+        }
+
+        public static void Divide()
+        {
+            Divide(10, 3, out int result, out int rest);
+            System.Console.WriteLine("{0} {1}", result, rest);  // Write "3 1"
+        }
     }
 }
